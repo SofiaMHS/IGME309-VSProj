439ce6c02aabd372609dd3990a37e2ecf81071d8
sam7920 <sam7920@g.rit.edu>
2025-09-14 15:48:22 -0400

    Commit for E03

--- E03/E03/exercise3.cpp
+++ E03/E03/exercise3.cpp
@@ -1,12 +1,79 @@
+#ifdef __APPLE__
+#include <GLUT/glut.h> // include glut for Mac
+#else
 #include <GL/freeglut.h> //include glut for Windows
+#endif
+
+
+
+int screenWidth;
+int screenHeight;
+
+
+void init(void) {
+
+    screenWidth = 400;
+    screenHeight = 400;
+
+}
+
+
+
+void reshape(int width, int height) {
+
+    //update screen dimensions
+    screenWidth = width;
+    screenHeight = height;
+
+
+    // do an orthographic parallel projection, limited by screen/window size
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(0.0, 10.0, 0.0, 10.0);
+
+    // tell OpenGL to use the whole window for drawing 
+    glViewport(0, 0, (GLsizei)width, (GLsizei)height);
+
+    glutPostRedisplay();
+
+
+}
 
 // called when the GL context need to be rendered
 void display(void)
 {
     // clear the screen to white, which is the background color
     glClearColor(1.0, 1.0, 1.0, 0.0);
+    // wipe the entire color buffer to the current clear color.
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // Default coordinate bounds
+    // x range: –1.0 (left) to +1.0 (right)
+    // y range: –1.0 (bottom) to +1.0 (top)
+    // z range: –1.0 (near) to +1.0 (far)
+
+    glColor3f(1.0f, 0.0f, 0.0f); // blue
+    glBegin(GL_TRIANGLES);
+    //left vertex
+    glVertex2f(-0.5f, 0.5f);
+
+
+    //right vertex
+    glVertex2f(0.5f, 0.5f);
+
+    //middle vertex
+    glVertex2f(0.0f, -0.5f);
+
+
+
     glEnd();
+
+    glutSwapBuffers();
+
 }
+
+
+
 
 int main(int argc, char* argv[])
 {
@@ -18,11 +85,22 @@ int main(int argc, char* argv[])
     // Color is speicfied to RGBA, four color channels with Red, Green, Blue and Alpha(depth)
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
 
-    //set the initial window size */
-    glutInitWindowSize(640, 480);
+    // set the initial window size 
+
+
+    glutInitWindowSize((int)screenWidth, (int)screenHeight);
+
+    //set the initial window size 
+    //glutInitWindowSize(400, 400);
 
     // create the window with a title
     glutCreateWindow("My First Triange");
+
+    // register function to handle window resizes
+    //glutReshapeFunc(reshape);
+
+    glutDisplayFunc(display);
+
 
     //start the glut main loop
     glutMainLoop();
