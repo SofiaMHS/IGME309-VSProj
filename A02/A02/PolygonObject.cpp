850b70fa744af574686bf69a9e336f5a392936ec
SofiaMHS <sam7920@g.rit.edu>
2025-10-04 15:23:46 -0400

    A02 - Setup

---
+++ A02/A02/PolygonObject.cpp
@@ -0,0 +1,26 @@
+#include "PolygonObject.h"
+
+PolyObject::PolyObject()
+{
+}
+
+PolyObject::~PolyObject()
+{
+}
+
+void PolyObject::addVertex(float x, float y)
+{
+}
+
+void PolyObject::setColor(float cr, float cg, float cb)
+{
+}
+
+unsigned int PolyObject::getVertNum()
+{
+	return 0;
+}
+
+void PolyObject::draw()
+{
+}
